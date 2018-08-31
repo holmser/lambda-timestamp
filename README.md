b70999bed3fc9ed56ffb95eab3504a955382e9ec
Chris Holmes <chlmes@amazon.com>
2018-08-31 15:24:48 -0600

    download file, add for loop

--- README.md
+++ README.md
@@ -4,12 +4,17 @@ This simple project checks the delay between an S3 object event and a lambda inv
 
 ## Usage
 
-```
+```sh
 
 sls deploy
 
-aws s3 cp file.txt s3://your-bucket-name/
+# for loop to put files up to S3
+for i in `seq 1 10`;
+do
+    aws s3 cp file.txt s3://lambda-timestamp/file$i.txt
+done
 
 sls logs -f hello 
+
 
 ```
--- handler.py
+++ handler.py
@@ -1,5 +1,6 @@
 import json
 import boto3
+import botocore
 import datetime
 import dateutil.parser
 
@@ -7,15 +8,32 @@ import dateutil.parser
 def lambda_handler(event, context):
     """get invocation time"""
     now = datetime.datetime.now()
-    
+    s3 = boto3.resource('s3')
+
+
     
     for record in event['Records']:
         """retrieve timestamp from s3 object"""
         s3_timestamp = dateutil.parser.parse(record['eventTime'])
         s3_timestamp = s3_timestamp.replace(tzinfo=None)
 
+        bucket = record['s3']['bucket']['name']
+        key = record['s3']['object']['key']
+
+        try:
+            s3.Bucket(bucket).download_file(key, '/tmp/file.txt')
+        except botocore.exceptions.ClientError as e:
+            if e.response['Error']['Code'] == "404":
+                print("The object does not exist.")
+            else:
+                raise
+
+        
+
+
     payload = {
         "statusCode": 200,
+        "object": "{}/{}".format(bucket, key),
         "s3TimeStamp":"{}".format(s3_timestamp),
         "lambdaTimeStamp": "{}".format(now),
         "delta": "{}".format(now-s3_timestamp),
